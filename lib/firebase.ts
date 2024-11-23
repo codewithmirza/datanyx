2e94401507201f5540b0b40f98c25ab437c4cae8
Mirza Anwaarullah Baig <manwaarullahb@gmail.com>
2024-11-24 04:23:33 +0530

    f error solved

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,16 +2,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBn4hmH0x583sKwfdRRrl0-IyVd4PLEH-4",
-    authDomain: "datanyx-2e035.firebaseapp.com",
-    projectId: "datanyx-2e035",
-    storageBucket: "datanyx-2e035.firebasestorage.app",
-    messagingSenderId: "506700731752",
-    appId: "1:506700731752:web:f49499d78b6e417256f2dd"
-  };
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app); 
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db }; 
